c19e466f1f56b4911eaa51f259d107d8d9e33616
odin-huawei <odin.araujo.barragan@gmail.com>
2021-04-25 18:54:53 -0500

    Se crea tabla tipo documento

--- src/main/resources/celix-prueba-schema.sql
+++ src/main/resources/celix-prueba-schema.sql
@@ -376,6 +376,30 @@ LOCK TABLES `tipo_usuario` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tipo_documento`
+--
+
+DROP TABLE IF EXISTS `tipo_documento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipo_documento` (
+  `id_tipo_documento` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(200) NOT NULL,
+  PRIMARY KEY (`id_tipo_documento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo_documento`
+--
+
+LOCK TABLES `tipo_documento` WRITE;
+/*!40000 ALTER TABLE `tipo_documento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipo_documento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
 -- Table structure for table `usuario`
 --
 
