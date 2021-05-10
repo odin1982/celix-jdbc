8aea7fa0569efce1474c0a88f31d45cbc7bbf1fa
odin-huawei <odin.araujo.barragan@gmail.com>
2021-05-09 23:57:09 -0500

    Compras - Se agrega modulo de compras

--- src/main/java/org/celix/controller/ComprasController.java
+++ src/main/java/org/celix/controller/ComprasController.java
@@ -1,6 +1,17 @@
 package org.celix.controller;
 
+import java.util.List;
+
+import org.celix.model.AlmacenModel;
+import org.celix.model.ComprasModel;
+import org.celix.model.ProveedorModel;
+import org.celix.model.TipoDocumentoModel;
+import org.celix.service.AlmacenService;
+import org.celix.service.ProveedorService;
+import org.celix.service.TipoDocumentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +19,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("operacion/compras")
 public class ComprasController {
 	
+	@Autowired private ProveedorService proveedorService;
+	@Autowired private TipoDocumentoService tipoDocumentoService;
+	@Autowired private AlmacenService almacenService;
+	
 	@GetMapping("/index")
-	public String index() {
+	public String index(Model model) {
+		ComprasModel compras = new ComprasModel();
+		List<ProveedorModel> proveedores = proveedorService.findAll();
+		List<TipoDocumentoModel> tiposDocumento = tipoDocumentoService.findAll();
+		List<AlmacenModel> almacenes = almacenService.findAll();
+		
+		model.addAttribute("compras", compras);
+		model.addAttribute("proveedores", proveedores);
+		model.addAttribute("tiposDocumento", tiposDocumento);
+		model.addAttribute("almacenes",almacenes);
 		return "operacion/compras/index";
 	}
 }
--- src/main/java/org/celix/model/ComprasModel.java
+++ src/main/java/org/celix/model/ComprasModel.java
@@ -3,6 +3,39 @@ package org.celix.model;
 public class ComprasModel {
 	private Long idProveedor;
 	private String numeroDocumento;
-	private Long tipoDocumento;
-
+	private Long idTipoDocumento;
+	private Long idAlmacen;
+	private String codigoProducto;
+	
+	public Long getIdProveedor() {
+		return idProveedor;
+	}
+	public void setIdProveedor(Long idProveedor) {
+		this.idProveedor = idProveedor;
+	}
+	public String getNumeroDocumento() {
+		return numeroDocumento;
+	}
+	public void setNumeroDocumento(String numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
+	public Long getIdTipoDocumento() {
+		return idTipoDocumento;
+	}
+	public void setIdTipoDocumento(Long idTipoDocumento) {
+		this.idTipoDocumento = idTipoDocumento;
+	}
+	public Long getIdAlmacen() {
+		return idAlmacen;
+	}
+	public void setIdAlmacen(Long idAlmacen) {
+		this.idAlmacen = idAlmacen;
+	}
+	public String getCodigoProducto() {
+		return codigoProducto;
+	}
+	public void setCodigoProducto(String codigoProducto) {
+		this.codigoProducto = codigoProducto;
+	}
+	
 }
