7c0aea1c7db74ae2481f446f7ceeec53f043783e
odin-huawei <odin.araujo.barragan@gmail.com>
2021-05-12 22:28:16 -0500

    Proveedor - Se agrega datatables a pantalla principal

---
+++ src/main/resources/static/js/proveedor/proveedor.js
@@ -0,0 +1,16 @@
+$(document).ready( function () {
+    $('#table_proveedor').DataTable({
+    	  "columnDefs": [
+    		    { "width": "5%",  "targets": 0 },
+    		    { "width": "10%", "targets": 1 },
+    		    { "width": "10%", "targets": 2 },
+    		    { "width": "10%", "targets": 3 },
+    		    { "width": "10%", "targets": 4 },
+    		    { "width": "10%", "targets": 5 },
+    		    { "width": "10%", "targets": 6 },
+    		    { "width": "10%", "targets": 7 },
+    		    { "width": "5%", "targets": 8,"orderable": false },
+    		    { "width": "5%", "targets": 9,"orderable": false } 
+    		  ]
+    });
+} );
