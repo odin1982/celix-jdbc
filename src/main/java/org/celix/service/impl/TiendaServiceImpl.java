3e067ed335a7adb7319e333372c9cde7afb930cb
odinLaptopUbuntu <odin.araujo.barragan@gmail.com>
2021-06-06 02:06:12 -0500

    Tienda - Se agregan validaciones de tienda

--- src/main/java/org/celix/service/impl/TiendaServiceImpl.java
+++ src/main/java/org/celix/service/impl/TiendaServiceImpl.java
@@ -2,9 +2,11 @@ package org.celix.service.impl;
 
 import java.util.List;
 
+import org.celix.model.ProveedorModel;
 import org.celix.model.TiendaModel;
 import org.celix.repository.TiendaRepository;
 import org.celix.service.TiendaService;
+import org.celix.util.MessagesPropertiesConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,9 @@ import org.springframework.stereotype.Service;
 public class TiendaServiceImpl implements TiendaService{
 	@Autowired
 	private TiendaRepository tiendaRepository;
+	
+	@Autowired
+	private MessagesPropertiesConfig messages;
 
 	@Override
 	public List<TiendaModel> findAll() {
@@ -29,18 +34,30 @@ public class TiendaServiceImpl implements TiendaService{
 	}
 
 	@Override
-	public void save(TiendaModel almacen) {
-		tiendaRepository.save(almacen);
+	public void save(TiendaModel tienda) {
+		validarCampos(tienda);
+		tiendaRepository.save(tienda);
 	}
 
 	@Override
-	public void update(TiendaModel almacen) {
-		tiendaRepository.update(almacen);
+	public void update(TiendaModel tienda) {
+		validarCampos(tienda);
+		tiendaRepository.update(tienda);
 	}
 
 	@Override
 	public void deleteById(Long id) {
 		tiendaRepository.deleteById(id);
+	}
+	
+	private void validarCampos(TiendaModel tienda) {
+		if( tienda.getNombre().trim().isEmpty() ) {
+			throw new IllegalArgumentException(messages.getProperty("celix.exceptions.argumento.invalido.nombre"));
+		}
+		
+		if( tienda.getDireccion().trim().isEmpty() ) {
+			throw new IllegalArgumentException(messages.getProperty("celix.exceptions.argumento.invalido.direccion"));
+		}
 	}
 
 }
