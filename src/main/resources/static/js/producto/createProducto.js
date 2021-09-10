5f4ddfd4dcaff9e012700e887b37c1828fc8ab03
odin-huawei <odin.araujo.barragan@gmail.com>
2021-09-09 22:09:33 -0500

    Producto-Se agrega validacion de precio

--- src/main/resources/static/js/producto/createProducto.js
+++ src/main/resources/static/js/producto/createProducto.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
+	$("#divPrecioCompra").removeClass('d-none');
+ 	$("#precioCompraIVA").addClass('d-none');
 	document.getElementById('precioCompra').disabled = false;
 	document.getElementById('precioCompraIVA').disabled = true;
 //jquery validation toma el atributo name, con thymeleaf en los select se toma th:field	
 $.validator.addMethod("validaSelect", function(value, element, arg){
   return arg != value;
  }, "Seleccione una opción");
+ 
+jQuery.validator.addMethod("validaPrecio", function(value, element,arg) {
+  // allow any non-whitespace characters as the host part
+  console.log("regex:"+(/(^[0-9]{1,5}$|^[0-9]{1,2}\.[0-9]{1,2}$)/.test( value )));
+  return this.optional( element ) || /(^[0-9]{1,5}$|^[0-9]{1,2}\.[0-9]{1,2}$)/.test( value );
+}, 'Please enter a valid email address.');
 
 	$("#createProducto").validate({
 		rules: {
@@ -12,12 +20,34 @@ $.validator.addMethod("validaSelect", function(value, element, arg){
 			descripcion: "required",
 			idTipoProducto: {validaSelect:"0"},
 			idMarca: {validaSelect:"0"},
+			idProveedor:{validaSelect:"0"},
+			precioCompra:{ 
+				required: function(element) {
+          			return !($("#precioCompraConIVA").is(":checked"));
+        		},
+				validaPrecio:true,
+			},
+			precioCompraIVA:{
+				required:function(element) {
+          			return $("#precioCompraConIVA").is(":checked");
+        		},
+				validaPrecio:true,
+			},
 		},
 		messages: {
 			nombre: "Campo obligatorio",
 			descripcion: "Campo obligatorio",
 			idTipoProducto: "Escoge una opción",
 			idMarca: "Escoge una opción",
+			idProveedor: "Escoge una opción",
+			precioCompra: {
+				required:"Campo obligatorio",
+				validaPrecio:"Precio Inválido"
+			},
+			precioCompraIVA: {
+				required:"Campo obligatorio",
+				validaPrecio:"Precio Inválido"
+			},
 		},
 		errorElement: "em",
 				errorPlacement: function ( error, element ) {
@@ -31,9 +61,15 @@ $.validator.addMethod("validaSelect", function(value, element, arg){
 					}
 				},
 				highlight: function ( element, errorClass, validClass ) {
+					console.log("element: " + element);
+					console.log("errorClass: " + errorClass);
+					console.log("validClass: " + validClass);
 					$( element ).addClass( "is-invalid" ).removeClass( "is-valid" );
 				},
 				unhighlight: function (element, errorClass, validClass) {
+					console.log("element: " + element);
+					console.log("errorClass: " + errorClass);
+					console.log("validClass: " + validClass);
 					$( element ).addClass( "is-valid" ).removeClass( "is-invalid" );
 				}
 	});
@@ -46,17 +82,25 @@ $.validator.addMethod("validaSelect", function(value, element, arg){
 
 });
 
-	
+//To show it: $("#myId").removeClass('d-none');
+//To hide it: $("#myId").addClass('d-none');
+//To toggle it: $("#myId").toggleClass('d-none');
 
 function changeStatusPrecioCompraConIVA(){
 		 if (document.getElementById('precioCompraConIVA').checked) { 
 			 document.getElementById('precioCompra').disabled = true;
+//			 document.getElementById('precioCompra').style.display = 'none';
 			 document.getElementById('precioCompra').value = "";
 			 document.getElementById('precioCompraIVA').disabled = false;
+ 			 $("#divPrecioCompra").addClass('d-none');
+ 			 $("#precioCompraIVA").removeClass('d-none');
 		 } 
 		 else {
 			 document.getElementById('precioCompra').disabled = false;
 			 document.getElementById('precioCompraIVA').disabled = true;
 			 document.getElementById('precioCompraIVA').value = "";
+//			 document.getElementById('precioCompra').style.display = 'block';
+ 			 $("#divPrecioCompra").removeClass('d-none');
+ 			  $("#precioCompraIVA").addClass('d-none');
 		 }
 }
